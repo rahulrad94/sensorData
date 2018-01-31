034f062e7c116649b0c8dd336da5e469809d884c
Rahul Radhakrishnan <rahul@Rahuls-MacBook-Pro.local>
2018-01-30 23:44:06 -0800

    1) Accelerometer added 2) GPS permission issue solved

--- app/src/main/java/com/example/rahul/gpsupdates/MainActivity.java
+++ app/src/main/java/com/example/rahul/gpsupdates/MainActivity.java
@@ -23,6 +23,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.hardware.SensorEventListener;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.hardware.SensorEvent;
+
 
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.common.api.ResolvableApiException;
@@ -53,7 +58,7 @@ import java.util.Date;
 import java.util.Locale;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener{
     private static final String TAG = "Sensor_Data_Updates";
 
     private static final int REQUEST_PERMISSIONS_REQUEST_CODE = 34;
@@ -68,19 +73,21 @@ public class MainActivity extends AppCompatActivity {
     private LocationCallback mLocationCallback;
     private Location mCurrentLocation;
 
+    private Sensor mySensor;
+    private SensorManager senMa;
+
     // UI Widgets.
-    private Button gpsStartButton;
-    private Button gpsStopButton;
-    private TextView mLastUpdateTimeTextView;
-    private TextView mLatitudeTextView;
-    private TextView mLongitudeTextView;
+    private Button mStartUpdatesButton;
+    private Button mStopUpdatesButton;
+    private TextView mLastUpdateTimeTextView, mLatitudeTextView, mLongitudeTextView;
+    private TextView xText, yText, zText;
 
     private Boolean mRequestingLocationUpdates;
     private String mLastUpdateTime;
 
     //mqtt server information
     static String mqttHost = "tcp://neptune.usc.edu:1883";
-    static String topicStr = "gpsUpdates";
+    static String topicStr = "gpsUpdates1";
 
     MqttAndroidClient client;
 
@@ -91,17 +98,24 @@ public class MainActivity extends AppCompatActivity {
 
         String clientId = MqttClient.generateClientId();
         client = new MqttAndroidClient(this.getApplicationContext(), mqttHost,
-                        clientId);
+                clientId);
 
         // UI
-        gpsStartButton = (Button) findViewById(R.id.start_updates_button);
-        gpsStopButton = (Button) findViewById(R.id.stop_updates_button);
+        mStartUpdatesButton = (Button) findViewById(R.id.start_updates_button);
+        mStopUpdatesButton = (Button) findViewById(R.id.stop_updates_button);
         mLatitudeTextView = (TextView) findViewById(R.id.latitude_text);
         mLongitudeTextView = (TextView) findViewById(R.id.longitude_text);
         mLastUpdateTimeTextView = (TextView) findViewById(R.id.last_update_time_text);
+        xText = (TextView) findViewById(R.id.x_text);
+        yText = (TextView) findViewById(R.id.y_text);
+        zText = (TextView) findViewById(R.id.z_text);
 
         mRequestingLocationUpdates = false;
         mLastUpdateTime = "";
+
+        senMa = (SensorManager) getSystemService(SENSOR_SERVICE);
+        mySensor = senMa.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        senMa.registerListener(this, mySensor, SensorManager.SENSOR_DELAY_NORMAL);
 
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
         mSettingsClient = LocationServices.getSettingsClient(this);
@@ -130,6 +144,18 @@ public class MainActivity extends AppCompatActivity {
         buildLocationSettingsRequest();
     }
 
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        xText.setText("X: " + sensorEvent.values[0]);
+        yText.setText("Y: " + sensorEvent.values[1]);
+        zText.setText("Z: " + sensorEvent.values[2]);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
     private void createLocationRequest() {
         mLocationRequest = new LocationRequest();
         mLocationRequest.setInterval(UPDATE_INTERVAL);
@@ -156,10 +182,29 @@ public class MainActivity extends AppCompatActivity {
         mLocationSettingsRequest = builder.build();
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            // Check for the integer request code originally supplied to startResolutionForResult().
+            case REQUEST_CHECK_SETTINGS:
+                switch (resultCode) {
+                    case Activity.RESULT_OK:
+                        Log.d(TAG, "User agreed to make required location settings changes.");
+                        // Nothing to do. startLocationupdates() gets called in onResume again.
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        Log.d(TAG, "User chose not to make required location settings changes.");
+                        mRequestingLocationUpdates = false;
+                        updateLocationUI();
+                        break;
+                }
+                break;
+        }
+    }
+
     public void startUpdatesButtonHandler(View view) {
         if (!mRequestingLocationUpdates) {
             mRequestingLocationUpdates = true;
-            //setButtonsEnabledState();
             startLocationUpdates();
         }
     }
@@ -177,7 +222,7 @@ public class MainActivity extends AppCompatActivity {
                         Log.i(TAG, "All location settings are satisfied.");
 
                         //noinspection MissingPermission
-                      mFusedLocationClient.requestLocationUpdates(mLocationRequest,
+                        mFusedLocationClient.requestLocationUpdates(mLocationRequest,
                                 mLocationCallback, Looper.myLooper());
 
                         updateLocationUI();
@@ -242,9 +287,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void stopLocationUpdates() {
-
         if (!mRequestingLocationUpdates) {
-            Log.d(TAG, "Updates never requested");
+            Log.d(TAG, "stopLocationUpdates: updates never requested, no-op.");
             return;
         }
 
@@ -260,10 +304,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onResume() {
         super.onResume();
-
-        if (mRequestingLocationUpdates) {
+        if (mRequestingLocationUpdates && checkPermissions()) {
             startLocationUpdates();
+        } else if (!checkPermissions()) {
+            requestPermissions();
         }
+
         updateLocationUI();
     }
 
@@ -273,23 +319,42 @@ public class MainActivity extends AppCompatActivity {
         stopLocationUpdates();
     }
 
+    private boolean checkPermissions() {
+        int permissionState = ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION);
+        return permissionState == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestPermissions() {
+        boolean shouldProvideRationale =
+                ActivityCompat.shouldShowRequestPermissionRationale(this,
+                        Manifest.permission.ACCESS_FINE_LOCATION);
+
+        if (shouldProvideRationale) {
+            Log.d(TAG, "Permission denied");
+        } else {
+            Log.d(TAG, "Requesting permission");
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_PERMISSIONS_REQUEST_CODE);
+        }
+    }
+
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        switch (requestCode) {
-            // Check for the integer request code originally supplied to startResolutionForResult().
-            case REQUEST_CHECK_SETTINGS:
-                switch (resultCode) {
-                    case Activity.RESULT_OK:
-                        Log.d(TAG, "User agreed to make required location settings changes.");
-                        // Nothing to do. startLocationupdates() gets called in onResume again.
-                        break;
-                    case Activity.RESULT_CANCELED:
-                        Log.d(TAG, "User chose not to make required location settings changes.");
-                        mRequestingLocationUpdates = false;
-                        updateLocationUI();
-                        break;
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        Log.i(TAG, "onRequestPermissionResult");
+        if (requestCode == REQUEST_PERMISSIONS_REQUEST_CODE) {
+            if (grantResults.length <= 0) {
+                Log.i(TAG, "User interaction was cancelled.");
+            } else if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (mRequestingLocationUpdates) {
+                    Log.i(TAG, "Permission granted, updates requested, starting location updates");
+                    startLocationUpdates();
                 }
-                break;
+            } else {
+                Log.i(TAG, "Permission denied");
+            }
         }
     }
 }
